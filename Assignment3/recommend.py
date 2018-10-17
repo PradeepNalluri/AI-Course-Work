ac6dde3b0052a89465cbd55d4e1cc510fc0e2656
PradeepNalluri <pradeepkumarnalluri557@gmail.com>
2018-10-17 23:41:24 +0530

    vector creation

--- Assignment3/recommend.py
+++ Assignment3/recommend.py
@@ -17,3 +17,35 @@ for i in range(len(uitem)):
     uitem[i]=map(str,uitem[i].split('|'))
     id=int(uitem[i][0])
     items[id]=str_list=map(int,feature_vect.split('|'))
+dataset=list()
+check=0
+for row in udata:
+    if(row[0]==check):
+        dataset[check-1].append([row[0],items[row[1]],row[-2]]  )
+    else:
+        dataset.append([row[0],items[row[1]],row[-2]])
+        check=row[0]
+
+# for user_set in dataset:
+#     test_len=int(0.3*len(user_set))
+#     train_len=len(user_set)-test_len
+#     count=0
+#     test_set=[]
+#     classes=[]
+#     while(count<test_len):
+#         rand_num=rand.randint(0,len(user_set)-1)
+#         if(user_set[rand_num] in test_set):
+#             pass
+#         else:
+#             test_set.append(user_set[rand_num])
+#             count+=1
+#     train_set=[]
+#     count=0
+#     for vect in user_set:
+#         if vect in test_set:
+#             count+=1
+#             pass
+#         else:
+#             train_set.append(vect)
+#     	if(vect[-1] not in classes):
+#     		classes.append(vect[-1])
